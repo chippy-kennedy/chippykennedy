24f70c7713882a5356b87c2cdebf09e78054795c
Chippy <chippy@case.edu>
2016-08-02 22:31:05 -0400

    font awesome, front page edits

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'https://rubygems.org'
 gem 'sinatra', '1.3.3'
 gem 'haml', '3.1.7'
 gem 'sass', '3.2.1'
+
+gem 'font-awesome-rails'
---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+ready = function(){
+
+		$(".menu").click( function() {
+			console.log("menu selected");
+			$(this).slideToggle(600);
+		});
+		
+		$(".menu-icon").click( function() {
+			console.log("menu-icon selected");
+			$(".menu").slideToggle(600);
+		});
+
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
